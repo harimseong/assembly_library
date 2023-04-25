30eac80eb77de0e9b9abdfbe627ca9edfa58f9df
Harim Seong <hseong@student.42seoul.kr>
2023-04-26 02:38:34 +0900

    x86 practice nasm style

---
+++ x86/hola_gcc.asm
@@ -0,0 +1,24 @@
+; ----------------------------------------------------------------------------------------
+; Writes "Hola, mundo" to the console using a C library. Runs on Linux.
+;
+;	 nasm -felf64 hola.asm && gcc hola.o && ./a.out
+; ----------------------------------------------------------------------------------------
+
+		global	main
+		extern	puts
+
+		section   .text
+main:									   ; This is called by the C library startup code
+		mov		rax, 1
+		mov		rdi, 1
+		mov		rsi, message
+		mov		rdx, 12
+		syscall
+		mov		rdi, message			; First integer (or pointer) argument in rdi
+		call	puts					; puts(message)
+		ret							   ; Return from main back into C library wrapper
+
+message:
+		db		"Hola, mundo", 10, 0		; Note strings must be terminated with 0 in C
+
+;https://cs.lmu.edu/~ray/notes/nasmtutorial/
