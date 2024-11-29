79fc98ff6e85dd25fec0e8db636db46761036d39
Harim Seong <halim9512@gmail.com>
2024-11-29 19:53:06 +0900

    ft_strlen.s: complete

--- lib/ft_strlen.s
+++ lib/ft_strlen.s
@@ -1,13 +1,28 @@
   section .text
-  global ft_strlen
+  global _ft_strlen
   align 16
 
-ft_strlen:
-  // new base pointer
-  push rbp
-  mov  rbp, rsp
+; size_t ft_strlen(const char* s);
+_ft_strlen:
+  ; new base pointer
+  push  rbp
+  mov   rbp, rsp
 
+  ; copy rdi.
+  mov   rax, rdi
+  dec   rax
 
+loop:
+  inc   rax
+  ; load 1 byte from the pointer.
+  mov   sil, byte [rax]
+
+  ; compare the byte with 0 and increment pointer.
+  ; if it is not 0, loop again.
+  cmp   sil, 0
+  jnz   loop
+
+  sub   rax, rdi
 
   pop rbp
   ret
