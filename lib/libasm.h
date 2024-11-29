6c260ec31d08aedf3c0079b4cd2d990ea68419b9
Harim Seong <halim9512@gmail.com>
2024-11-29 22:50:54 +0900

    libasm.h: prototypes

---
+++ lib/libasm.h
@@ -0,0 +1,10 @@
+#include <sys/_types/_ssize_t.h>
+#include <stddef.h>
+
+size_t  ft_strlen(const char * s);
+char *  ft_strcpy(char * dst, const char * src);
+int     ft_strcmp(const char * s1, const char * s2);
+char*   ft_strdup(const char * s);
+
+ssize_t ft_read(int fd, void * buf, size_t nbyte);
+ssize_t ft_write(int fd, const void * buf, size_t nbyte);
