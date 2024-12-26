a32454f5b4c74c179d53f094abbfe63731687a77
Harim Seong <halim9512@gmail.com>
2024-12-27 06:58:57 +0900

    merge sort list: left_end and right_end need to be fixed

--- lib/merge_sort_list.c
+++ lib/merge_sort_list.c
@@ -5,62 +5,71 @@ typedef struct s_list {
   struct s_list*  next;
 } t_list;
 
-void  ft_swap(void* left, void* right);
-int   ft_list_size(t_list* head);
-void  divide(t_list** head, int size, int (*cmp)(void*, void*));
-void  merge(t_list** right, t_list** left, int (*cmp)(void*, void*));
+void  ft_swap(void** left, void** right);
+void  divide(t_list** head, t_list* end, size_t size, int (*cmp)(void*, void*));
+void  merge(t_list** left, t_list** right, t_list* end, int (*cmp)(void*, void*));
 
 void  merge_sort_list(t_list** head, int (*cmp)(void*, void*))
 {
   t_list* node;
-  int     size = ft_list_size(*head);
+  size_t  size = 0;
 
-  divide(head, size, cmp);
+  node = *head;
+  while (node != 0) {
+    node = node->next;
+    ++size;
+  }
+  if (size <= 1) {
+    return;
+  }
+  divide(head, node, size, cmp);
 }
 
-void  divide(t_list** head, int size, int (*cmp)(void*, void*))
+void  divide(t_list** head, t_list* end, size_t size, int (*cmp)(void*, void*))
 {
   if (size <= 2) {
-    if (size == 2) {
-      t_list* node = *head;
+    t_list* node = *head;
+
+    if (size == 2 && cmp(node->data, node->next->data) > 0) {
 
       ft_swap(&node->data, &node->next->data);
     }
     return;
   }
-  int     half_size = size >> 1;
+  size_t   half_size = size >> 1;
   t_list** middle = head;
 
-  while (half_size > 0) {
+  for (size_t i = 0; i < half_size; ++i) {
     middle = &(*middle)->next;
-    --half_size;
   }
-  divide(head, half_size, cmp);
-  divide(middle, size - half_size, cmp);
-  merge(head, middle, cmp);
+  divide(head, *middle, half_size, cmp);
+  divide(middle, end, size - half_size, cmp);
+  merge(head, middle, end, cmp);
 }
 
-void  merge(t_list** right, t_list** left, int (*cmp)(void*, void*))
+void  merge(t_list** left, t_list** right, t_list* end, int (*cmp)(void*, void*))
 {
-  t_list* right_end = *left;
+  t_list* left_end = *right;
 
-  while (right != right_end && left != 0) {
-    if (cmp(left->data, right->data) > 0) {
+  while (1) {
+    t_list* l = *left;
+    t_list* r = *right;
 
-      right = right->next;
-    } else {
-      left = left->next;
+    if (l == 0 || r == 0 || l->next == left_end || r->next == end) {
+      break;
     }
+    if (cmp(l->data, r->data) > 0) {
+      *right = r->next;
+      *left = r;
+      r->next = l;
+    }
+    left = &(*left)->next;
   }
 }
 
-int ft_list_size(t_list* head)
+void  ft_swap(void** left, void** right)
 {
-  int size = 0;
-
-  while (head != NULL) {
-    head = head->next;
-    ++size;
-  }
-  return size;
+  void* temp = *left;
+  *left = *right;
+  *right = temp;
 }
---
+++ lib/merge_sort_list_test.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define SIZE (9)
+
+typedef struct s_list {
+  void*           data;
+  struct s_list*  next;
+} t_list;
+
+typedef int t_num;
+
+void merge_sort_list(t_list** head, int (*cmp)(void*, void*));
+int cmp(void* l, void* r);
+
+int main(int argc, char** argv)
+{
+  t_list* head = 0;
+  t_list** ptr = &head;
+
+  if (argc > 1) {
+    srand(atoi(argv[1]));
+  }
+  for (int i = 0; i < SIZE; ++i) {
+    *ptr = malloc(sizeof(t_list));
+    (*ptr)->data = (void*)(rand() % 128lu);
+    ptr = &(*ptr)->next;
+  }
+  *ptr = 0;
+
+  ptr = &head;
+  while (*ptr != 0) {
+    printf("ptr=%p, *ptr=%p, *ptr->next=%p, *ptr->data=%ld\n", 
+        ptr, *ptr, (*ptr)->next, (long int)(*ptr)->data);
+    ptr = &(*ptr)->next;
+  }
+
+  merge_sort_list(&head, cmp);
+
+  int is_fail = 0;
+
+  printf("%ld ", (long int)head->data);
+  for (int i = 1; i < SIZE; ++i) {
+    printf("%ld ", (long int)head->next->data);
+    if (head->data > head->next->data) {
+      is_fail = 1;
+    }
+  }
+  if (is_fail) {
+    printf("FAIL\n");
+  } else {
+    printf("SUCCESS\n");
+  }
+}
+
+int cmp(void* l, void* r)
+{
+  return l - r;
+}
