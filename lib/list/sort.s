aac7049105e5e9c8a96d3ca25179753d2b68bfb2
Harim Seong <halim9512@gmail.com>
2025-01-01 15:06:39 +0900

    finished writing merge sort in assembly

--- lib/list/sort.s
+++ lib/list/sort.s
@@ -21,6 +21,7 @@ loop0:
 
   inc   rdx
   mov   rax, [rax + 8]
+  jmp   loop0
 loop0_end:
 
   mov   rcx, rsi
@@ -70,24 +71,104 @@ divide2:
   push  rbp
   mov   rbp, rsp
 
-  sub   rsp, 24
-  mov   [rsp], rbx
-  mov   [rsp + 8], r12
-  mov   [rsp + 16], r13
-  mov   rbx, rdi
-  mov   r12, rsi
+  sub   rsp, 40
+  mov   [rsp], rdi
+  mov   [rsp + 8], rsi
+  mov   [rsp + 16], rdx
+  mov   [rsp + 24], rcx
 
+  mov   rsi, [rdi]
+  xor   rax, rax
+  shr   rdx, 1
+divide2_loop:
+  cmp   rax, rdx
+  je    divide2_loop_end
 
+  mov   rsi, [rsi + 8]
+  inc   rax
+  jmp   divide2_loop
 
+divide2_loop_end:
+  sub   [rsp + 16], rax
+  mov   rdi, [rsp]
+  mov   rdx, rax
+  call  divide
+
+  mov   [rsp + 32], rax
+  mov   rsi, [rsp + 8]
+  mov   rdx, [rsp + 16]
+  mov   rcx, [rsp + 24]
+  mov   rdi, rax
+  call  divide
+
+  mov   rdi, [rsp]
+  mov   rsi, [rsp + 32]
+  mov   rdx, [rsp + 16]
+  mov   rcx, [rsp + 24]
+  call  merge
 ;
 divide_ret:
-  mov   rbx, [rsp]
-  mov   r12, [rsp + 8]
-  mov   r13, [rsp + 16]
   mov   rsp, rbp
   pop   rbp
   ret
 
-
-; t_list** merge(t_list** left, t_list* right, t_list* end, int (*cmp)(void*, void*));
+; t_list** merge(t_list** left, t_list** right, t_list* end, int (*cmp)(void*, void*));
 merge:
+  push  rbp
+  mov   rbp, rsp
+
+  sub   rsp, 32
+  mov   [rsp], rbx
+  mov   [rsp + 8], r12
+  mov   [rsp + 16], r13
+  mov   [rsp + 24], r14
+
+  mov   rbx, rdi
+  mov   r12, rsi
+  mov   r13, rdx
+  mov   r14, rcx
+merge_loop:
+  mov   r8, [rbx] ; l = *left
+  mov   r9, [r12] ; r = *right
+
+  cmp   r8, r9
+  jge   merge_loop2
+  cmp   r9, rdx
+  je    merge_loop2
+
+  mov   rdi, [r8]
+  mov   rsi, [r9]
+  call  r14
+
+  cmp   rax, 0
+  jle   merge_loop1
+
+  mov   rax, [r9 + 8]
+  mov   [r12], rax     ; *right = r->next
+  mov   [r9 + 8], r8   ; r->next = l
+  mov   [rbx], r9      ; *left = r
+
+merge_loop1:
+  mov   rbx, [rbx]
+  lea   rbx, [rbx + 8] ; left = &(*left)->next
+  jmp   merge_loop
+
+merge_loop2:
+  cmp   [r12], r13     ; *right != end
+  je    merge_ret
+
+  mov   r12, [r12]
+  lea   r12, [r12 + 8] ; right = &(*right)->next
+  jmp   merge_loop2
+
+merge_ret:
+  mov   rax, r12
+  mov   rbx, [rsp]
+  mov   r12, [rsp + 8]
+  mov   r13, [rsp + 16]
+  mov   r14, [rsp + 24]
+
+  mov   rsp, rbp
+  pop   rbp
+  ret
+
