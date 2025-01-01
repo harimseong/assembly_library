ad0679ac8e29b6084c0069b773260bbf42d6ea17
Harim Seong <halim9512@gmail.com>
2025-01-01 22:00:44 +0900

    all basic test has been passed

--- lib/test_list.c
+++ lib/test_list.c
@@ -1,5 +1,13 @@
+#include <string.h>
+#include <stdlib.h>
+
 #include "test.h"
 #include "list.h"
+
+typedef struct s_data {
+  char* str;
+  int* arr;
+} t_data;
 
 void ft_list_push_front(t_list** head, void* data);
 int ft_list_size(t_list* head);
@@ -12,7 +20,11 @@ void ft_list_remove_if(t_list** head,
 void  test_list_push_front_and_size();
 void  test_list_sort_boundary();
 void  test_list_remove_if();
-extern int   cmp(void* l, void* r);
+extern int cmp(void*, void*);
+
+int   cmp_data(void* l, void* r);
+t_data* alloc_data(char* str, int* arr);
+void    free_data(void* data);
 
 void test_list(void)
 {
@@ -29,19 +41,23 @@ void  test_list_push_front_and_size()
 
   t_list* head = 0;
   for (int i = 0; i < size; ++i) {
-    if (ft_list_size(head) != i) {
+    int size;
+    if ((size = ft_list_size(head)) != i) {
       is_fail = 1;
       break;
     }
+    printf("i: size=%d, ", size);
     ft_list_push_front(&head, (void*)arr[i]);
     t_list* node = head;
     for (int j = i; j >= 0; --j) {
+      printf("%ld ", (long int)node->data);
       if (node->data != (void*)arr[j]) {
         is_fail = 1;
         break;
       }
       node = node->next;
     }
+    printf("\n");
     if (node != 0) {
       is_fail = 1;
       break;
@@ -72,6 +88,87 @@ void  test_list_sort_boundary()
 void  test_list_remove_if()
 {
   int is_fail = 0;
+  int num = 42;
+  t_data ref = (t_data){
+    .str = "hello",
+    .arr = &num
+  };
+  char* strs[10] = {
+    "hello", "bbb", "ccc", "ddd",
+    "eee", "hello", "ggg", "hhh",
+    "iii", "jjj",
+  };
+  int   nums[10] = {0, 42, 2, 3, 4, 42, 6, 7, 8, 42};
+  char* strs_after[6] = {
+    "ccc", "ddd", "eee",
+    "ggg", "hhh", "iii", 
+  };
+  int   nums_after[6] = {2, 3, 4, 6, 7, 8};
 
+  t_list* head = 0;
+
+  printf("before:\n");
+  for (int i = 0; i < 10; ++i) {
+    t_data* data = alloc_data(strs[i], nums + i);
+    ft_list_push_front(&head, data);
+    printf("data->str=%s, *data->arr=%d\n", data->str, *data->arr);
+  }
+  printf("\n");
+  ft_list_remove_if(&head, &ref, cmp_data, free_data);
+
+  t_list* node = head;
+  printf("after:\n");
+  for (int i = 5; i >= 0; --i) {
+    t_data* data = node->data;
+    printf("data->str=%s, *data->arr=%d\n", data->str, *data->arr);
+    if (strcmp(data->str, strs_after[i]) != 0 ||
+        *data->arr != nums_after[i]) {
+      is_fail = 1;
+      break;
+    }
+    node = node->next;
+  }
+  printf("\n");
   TEST_RESULT(is_fail);
+}
+
+void free_data(void* data)
+{
+  t_data* d = data;
+
+  free(d->str);
+  free(d->arr);
+  free(d);
+}
+
+t_data* alloc_data(char* str, int* arr)
+{
+  t_data*  data;
+  char* new_str;
+  int* new_arr;
+
+  data = malloc(sizeof(t_data));
+  if (data == 0) {
+    return 0;
+  }
+  new_str = strdup(str);
+  new_arr = malloc(sizeof(int));
+  *new_arr = *arr;
+  data->str = new_str;
+  data->arr = new_arr;
+  return data;
+}
+
+int   cmp_data(void* l, void* r)
+{
+  t_data* l_data = l;
+  t_data* r_data = r;
+
+  if (strcmp(l_data->str, r_data->str) == 0) {
+    return 0;
+  }
+  if (*l_data->arr == *r_data->arr) {
+    return 0;
+  }
+  return 1;
 }
